68b484dcbab3e03556300c89db1a54a42472cd59
Azkoz1 <arthurfrisach347@gmail.com>
2026-08-10 23:30:48 +0200

    Ajout du mur des donateurs

---
+++ donors.js
@@ -0,0 +1,12 @@
+// Mur des donateurs
+// Pour ajouter quelqu'un : copie une ligne, change le pseudo et le nom de fichier image,
+// et mets la photo correspondante dans assets/donors/.
+const DONORS = [
+  { pseudo: "@yamapidou", image: "assets/donors/yamapidou.webp" },
+  { pseudo: "@tadomikiri", image: "assets/donors/tadomikiri.webp" },
+  { pseudo: "@travis_.dev", image: "assets/donors/travis_dev.webp" },
+  { pseudo: "@djodjokz", image: "assets/donors/djodjokz.webp" },
+  { pseudo: "@ism.9", image: "assets/donors/ism9.webp" },
+  { pseudo: "@concombre_3d", image: "assets/donors/concombre_3d.webp" },
+  { pseudo: "@midas0410", image: "assets/donors/midas0410.webp" },
+];
